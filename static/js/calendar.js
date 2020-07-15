13b87dbb98a68aea0f894cc3a37f5af8055bc11c
Toogii <dashdava@uw.edu>
2020-07-15 11:33:56 -0700

    Testing

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -16,56 +16,59 @@ function goToCalendar() {
 
     function buildCalendar() {
         console.log("Mobile:Building the Calendar")
+        var date = new Date(); 
+        var firstDay = new Date(date.getFullYear(), date.getMonth(), 1); 
+        // let closestWeekend = moment($("#currentDay").text()).startOf('month').day(-1);
+        var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0); 
+        // let gap = firstDay.diff(closestWeekend, 'days');
 
-        let firstDay = moment($("#currentDay").text()).startOf('month');
-        let closestWeekend = moment($("#currentDay").text()).startOf('month').day(-1);
-        let lastDay = moment($("#currentDay").text()).endOf('month');
-        let gap = firstDay.diff(closestWeekend, 'days');
-
-        if (firstDay === "Invalid date" || lastDay === "Invalid date") {
-            console.log("Mobile:Invalid date found")
-            return "Escape";
-        }
-        if (gap < 7) {
-            for (i=0;i<gap;i++) {
-                calendarAppDiv.append(`<div class='day past-month' data-date='past-month'>${closestWeekend.format("ddd DD")}</div>`);
-                closestWeekend.add(1, 'day');
-            }
-        }
-        let activity;
-        while (!firstDay.isAfter(lastDay)) {
-            let day = firstDay.format("D");
-            let month = firstDay.format("MMMM");
-            let year = firstDay.format("YYYY");
-            let dateChosen = month + " " + day + "," + " " + year;
-            // console.log(dateChosen);
-            for (index=0;index<10;index++) {
-                if (localStorage.getItem(dateChosen + "-" + index)) {
-                    activity = "<br><strong style='color: red;'>Note Exist</strong>";
-                    // console.log(dateChosen + "-" + index);
-                    // console.log(localStorage.getItem(dateChosen + "-" + index));
-                    break;
-                }
-                else {
-                    activity = "";
-                }
-            }
+        // if (firstDay === "Invalid date" || lastDay === "Invalid date") {
+        //     console.log("Mobile:Invalid date found")
+        //     return "Escape";
+        // }
+        // if (gap < 7) {
+        //     for (i=0;i<gap;i++) {
+        //         calendarAppDiv.append(`<div class='day past-month' data-date='past-month'>${closestWeekend.format("ddd DD")}</div>`);
+        //         closestWeekend.add(1, 'day');
+        //     }
+        // }
+        // let activity;
+        let activity = "done";
+        var day = firstDay.getDate();
+        while (day !== lastDay.getDate()) {
+            // let day = firstDay.format("D");
+            // let month = firstDay.format("MMMM");
+            // let year = firstDay.format("YYYY");
+            // let dateChosen = month + " " + day + "," + " " + year;
+            // // console.log(dateChosen);
+            // for (index=0;index<10;index++) {
+            //     if (localStorage.getItem(dateChosen + "-" + index)) {
+            //         activity = "<br><strong style='color: red;'>Note Exist</strong>";
+            //         // console.log(dateChosen + "-" + index);
+            //         // console.log(localStorage.getItem(dateChosen + "-" + index));
+            //         break;
+            //     }
+            //     else {
+            //         activity = "";
+            //     }
+            // }
+            var dateChosen = firstDay.toDateString().split(" ")[1] + " " + day + "," + " " + firstDay.toDateString().split(" ")[3];
             if (isWeekend(dateChosen)) {
-                calendarAppDiv.append(`<div class="day weekend">${firstDay.format("ddd DD")} ${activity}</div>`);
+                calendarAppDiv.append(`<div class="day weekend">${day} ${activity}</div>`);
             }
             else if (isFutureDay(dateChosen)) {
-                calendarAppDiv.append(`<div class="day future">${firstDay.format("ddd DD")} ${activity}</div>`);
+                calendarAppDiv.append(`<div class="day future">${day} ${activity}</div>`);
             }
             else if (isPresentDay(dateChosen)) {
-                calendarAppDiv.append(`<div class="day present">${firstDay.format("ddd DD")} ${activity}</div>`);
+                calendarAppDiv.append(`<div class="day present">${day} ${activity}</div>`);
             }
             else if (isPastDay(dateChosen)) {
-                calendarAppDiv.append(`<div class="day past">${firstDay.format("ddd DD")} ${activity}</div>`);
+                calendarAppDiv.append(`<div class="day past">${day} ${activity}</div>`);
             }
             else {
-                calendarAppDiv.append(`<div class="day">${firstDay.format("ddd DD")} ${activity}</div>`);
+                calendarAppDiv.append(`<div class="day">${day} ${activity}</div>`);
             }
-            firstDay.add(1, 'day');
+            day++;
             
         }
     }
